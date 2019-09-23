c3b04544e68ab99b572915e1cc07c5aac16c5630
oMalyugina <o.v.malyugina@gmail.com>
2019-09-23 13:24:29 +0200

    init kitti2ros article

---
+++ kitti2ros/code/include/Converter.h
@@ -0,0 +1,8 @@
+//
+// Created by olga on 23.09.19.
+//
+
+#ifndef ARTICLES_CONVERTER_H
+#define ARTICLES_CONVERTER_H
+
+#endif //ARTICLES_CONVERTER_H
---
+++ kitti2ros/code/src/convertKitti2rosForCartogropher.cpp
@@ -0,0 +1,4 @@
+//
+// Created by olga on 23.09.19.
+//
+
